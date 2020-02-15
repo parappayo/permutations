35f075d8c32cb8caef509538fe9694cabef26d6d
Jason Estey <esteyj@yahoo.com>
2020-02-14 17:09:33 -0800

    minor refactor

--- permutations.py
+++ permutations.py
@@ -2,13 +2,13 @@ import sys
 from typing import List
 
 
-def permutations(nums: List[int]) -> List[int]:
-    if len(nums) < 2:
-        yield nums
+def permutations(input: List[int]) -> List[int]:
+    if len(input) < 2:
+        yield input
         return
-    for sub_perm in permutations(nums[1:]):
-        for i in range(len(nums)):
-            yield sub_perm[:i] + nums[0:1] + sub_perm[i:]
+    for sub_perm in permutations(input[1:]):
+        for i in range(len(input)):
+            yield sub_perm[:i] + input[0:1] + sub_perm[i:]
 
 
 if __name__ == "__main__":
